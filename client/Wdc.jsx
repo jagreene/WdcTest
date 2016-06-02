68a116bc83e69e97afb98acef212ae0d05f40f31
jagreene <austin.greene@students.olin.edu>
2016-06-02 13:17:48 -0400

    rolling back example code more

--- client/Wdc.jsx
+++ client/Wdc.jsx
@@ -12,95 +12,29 @@ class Wdc extends Component{
     constructor(props) {
         super(props);
         // Create the connector object
-
         this.connector = tableau.makeConnector();
-        this.connector.getSchema = function(schemaCallback) {
-            var cols = [{
-                id: "mag",
-                alias: "magnitude",
-                dataType: tableau.dataTypeEnum.float
-                }, {
-                id: "title",
-                alias: "title",
-                dataType: tableau.dataTypeEnum.string
-                }, {
-                id: "url",
-                alias: "url",
-                dataType: tableau.dataTypeEnum.string
-            }, {
-                id: "lat",
-                alias: "latitude",
-                dataType: tableau.dataTypeEnum.float
-            }, {
-                id: "lon",
-                alias: "longitude",
-                dataType: tableau.dataTypeEnum.float
-            }];
 
-            var tableInfo = {
-                id: "earthquakeFeed",
-                alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
-                columns: cols
+        //define schema getter
+        this.connector.getSchema = (cb) => {
+            let tableInfo = {
+                id: this.props.id || "tableId",
+                alias: this.props.alias || "tableAlias",
+                columns: this.props.cols || []
             };
 
-            schemaCallback([tableInfo]);
+            console.log(tableInfo);
+            cb([tableInfo]);
         };
 
-        // Download the data
-        // this.connector.getData = function(table, doneCallback) {
-        //     var mag = 0,
-        //         title = "",
-        //         url = "",
-        //         lat = 0,
-        //         lon = 0;
-
-        //         $.getJSON("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-        //         var feat = resp.features,
-        //         tableData = [];
-
-        //         // Iterate over the JSON object
-        //         for (var i = 0, len = feat.length; i < len; i++) {
-        //             mag = feat[i].properties.mag;
-        //             title = feat[i].properties.title;
-        //             url = feat[i].properties.url;
-        //             lon = feat[i].geometry.coordinates[0];
-        //             lat = feat[i].geometry.coordinates[1];
-
-        //             tableData.push({
-        //                 "mag": mag,
-        //                 "title": title,
-        //                 "url": url,
-        //                 "lon": lon,
-        //                 "lat": lat
-        //             });
-
-        //             }
-
-        //             table.appendRows(tableData);
-        //             doneCallback();
-        //     });
-        // };
-
-        // this.connector.getSchema = (cb) => {
-        //     let tableInfo = {
-        //         id: this.props.id || "tableId",
-        //         alias: this.props.alias || "tableAlias",
-        //         columns: this.props.cols || []
-        //     };
-
-        //     console.log(tableInfo);
-        //     cb([tableInfo]);
-        // };
-
+        //attach user function for getting data
         this.connector.getData = this.props.getData;
+        //do any on click functionality user provides, then submit
         this.handleClick = event => {
             if (this.props.handleClick){
                 this.props.handleClick();
             }
             tableau.connectionName = this.props.connectionName || "connection";
             tableau.submit();
-                // tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
-                    // tableau.submit(); // This sends the connector object to Tableau
         };
         tableau.registerConnector(this.connector);
     }
