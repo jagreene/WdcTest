0c09df27b945f34b892d3c8626f9dcb4c6e33b92
jagreene <austin.greene@students.olin.edu>
2016-06-06 14:10:51 -0400

    added user model file

---
+++ models/users.js
@@ -0,0 +1,30 @@
+var mongoose = require("mongoose") ;
+
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var usersSchema = mongoose.Schema({
+    name: String,
+    fbId: String,
+    feed: {}
+});
+
+usersSchema.statics.findOrCreate = function (findParams, userData){
+    return new Promise((resolve, reject) =>{
+        this.find(findParams, (err, users) =>{
+            if (err) {reject(err)}
+            else {
+                if (users.length === 0){
+                    var newUser = new this(Object.assign(userData, findParams))
+                    resolve(newUser.save())
+                } else if (users.length === 1){
+                    resolve(users[0])
+                } else {
+                    reject(new Error("Multiple objects meet find criteria"))
+                }
+            }
+        })
+    })
+}
+
+module.exports = mongoose.model('User', usersSchema);
