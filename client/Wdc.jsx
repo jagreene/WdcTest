f66accbf0202fd8353b844e98f2285253c2ba4e1
jagreene <austin.greene@students.olin.edu>
2016-06-06 15:44:40 -0400

    loggging

--- client/Wdc.jsx
+++ client/Wdc.jsx
@@ -50,7 +50,7 @@ class Wdc extends Component{
 
         //attach user function for getting data
         this.connector.getData = (table, doneCallback) => {
-            tablea.abortWithError(this.props);
+            tableau.abortWithError(this.props);
             fetch(`http://wdc-react.heroku.com/api/user?userId=${tableau.password}`, {credentials:'same-origin'})
             .then(data => {
                 table.appendRows(this.props.gatherCallback(data));
